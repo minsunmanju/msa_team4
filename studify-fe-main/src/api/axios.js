a68b918b983427d2c975689c6e19aeb1b9aff11c
강채민 <rkdcoals23@naver.com>
2025-10-27 15:12:06 +0900

    Initial commit - MSA team4 project

---
+++ studify-fe-main/src/api/axios.js
@@ -0,0 +1,17 @@
+// src/api/axios.js
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: '/',
+  withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
